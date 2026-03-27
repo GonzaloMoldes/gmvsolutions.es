184375c2d799ba36d778cf973e723585d18ee59e
Gonzalo Moldes <gonzalomoldesvieites@gmail.com>
2026-03-27 04:02:00 +0100

    Actualizacion de pagina

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://gmvsolutions.es',
+  site: 'https://www.gmvsolutions.es',
   trailingSlash: 'always',
   integrations: [sitemap()],
 });
